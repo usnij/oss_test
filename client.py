67ee00ab64566f25b20ed8192631501c269311fc
jsmo <121493812+usnij@users.noreply.github.com>
2023-10-04 11:19:09 +0900

    Add files via upload

---
+++ client.py
@@ -0,0 +1,12 @@
+from socket import *
+serverPort = 12000
+
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(('', serverPort))
+print('Ther server is ready to receive')
+
+while True:
+    message, clientAddress = serverSocket.recvfrom(2048)
+    print(message.decode())
+    modifiedMessage = message.decode().upper()
+    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
